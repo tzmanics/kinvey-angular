a75300a220c0c9037a2fcbc9571c373f1e93c2f9
tzmanics <tzmanics@gmail.com>
2018-12-26 23:16:51 -0500

    delete initial filler & fix formatting

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,7 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to kinvey-angular!');
+    expect(compiled.querySelector('h1').textContent)
+      .toContain('Welcome to kinvey-angular!');
   });
 });
