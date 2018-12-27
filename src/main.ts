8cf41d175ed1fd4552f6009d05049146df305e9f
tzmanics <tzmanics@gmail.com>
2018-12-26 23:08:37 -0500

    add kinvey connection

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,25 @@
+import { Kinvey } from 'kinvey-angular2-sdk';
+
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { kinveyKeys } from './keys';
 
 if (environment.production) {
   enableProdMode();
 }
+
+Kinvey.init(kinveyKeys);
+
+Kinvey.ping()
+  .then((response) => {
+    console.log(`version: ${response.version} ${response.kinvey}`);
+  })
+  .catch((error) => {
+    console.log(`Kinvey Ping Failed. Response: ${error.description}`);
+  });
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
